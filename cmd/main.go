f0d9ff8b1dc9e40d45a23d0a4098aa577141dcc9
Sergey Bronnikov <estetus@gmail.com>
2025-05-05 17:43:29 +0300

    Fix random seed
    The patch contains the following changes:
    
    - The `rand.Seed` is deprecated, as of Go 1.24 `Seed` is a no-op [1].
      The patch replaces `rand.Seed()` with `New(NewSource(seed))`.
    - Random seed is generated, if 0 is specified. By default seed
      is 0.
    
    1. https://pkg.go.dev/math/rand#Seed

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
+
+	rand "math/rand"
 
 	gromit "github.com/ligurio/gromit"
 	"golang.org/x/exp/ebnf"
@@ -16,7 +19,7 @@ var (
 	name    = flag.String("filename", "", "filename with grammar")
 	action  = flag.String("action", "fuzz", "action (possible values: fuzz and dict)")
 	start   = flag.String("start", "", "start string")
-	seed    = flag.Int64("seed", -1, "number used to initialize a pseudorandom number generator")
+	seed    = flag.Int64("seed", 0, "random seed; if 0, seed is generated (default)")
 	maxreps = flag.Int("maxreps", 10, "maximum number of repetitions")
 	depth   = flag.Int("depth", 30, "maximum depth")
 	padding = flag.String("padding", " ", "non-terminal padding characters")
@@ -24,6 +27,12 @@ var (
 
 func main() {
 	flag.Parse()
+
+	if *seed == 0 {
+		*seed = time.Now().UnixNano()
+	}
+	source := rand.NewSource(*seed)
+	rng := rand.New(source)
 
 	if *name == "" && *start == "" {
 		flag.Usage()
@@ -51,7 +60,7 @@ func main() {
 	}
 
 	if *action == "dict" {
-		err = gromit.Dict(os.Stdout, grammar, *start, *seed)
+		err = gromit.Dict(os.Stdout, grammar, *start, rng)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -59,7 +68,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = gromit.Random(os.Stdout, grammar, *start, *seed, *maxreps)
+	err = gromit.Random(os.Stdout, grammar, *start, rng, *maxreps)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
